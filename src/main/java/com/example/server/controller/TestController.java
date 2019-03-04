84476d5266190dc7fce07d56c7e00f9722ad1f34
lfm <1071956678@qq.com>
2019-03-05 00:28:40 +0800

    Revert "数据库连接"
    This reverts commit 7ab537fe4ab3673ade0a9f13ee67ce576fea2b16.

--- src/main/java/com/example/server/controller/TestController.java
+++ src/main/java/com/example/server/controller/TestController.java
@@ -1,7 +1,6 @@
 package com.example.server.controller;
 
 import com.example.server.mapper.UserMapper;
-import com.example.server.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
     @Autowired
-    private UserService userService;
+    private UserMapper userMapper;
 
     @GetMapping("/getUser")
     public String get() {
-        return userService.get();
+        return userMapper.selectByPrimaryKey("AR").getUserid();
     }
 }
