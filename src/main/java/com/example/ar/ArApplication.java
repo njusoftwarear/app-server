516d33da08ae1772d721cd0ed49dde6d30be7209
lfm <1071956678@qq.com>
2019-03-05 00:43:56 +0800

    不熬夜了 肝不动 环境还不好

--- src/main/java/com/example/ar/ArApplication.java
+++ src/main/java/com/example/ar/ArApplication.java
@@ -2,12 +2,14 @@ package com.example.ar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ArApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ArApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ArApplication.class, args);
+    }
 
 }
