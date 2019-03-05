2f6631013c8147c05d638aef396f511fcb13c89e
lfm <1071956678@qq.com>
2019-03-05 19:19:43 +0800

    完成部署，ip: http://106.14.140.93:8080/server

--- src/main/java/com/ar/main/MainApplication.java
+++ src/main/java/com/ar/main/MainApplication.java
@@ -1,13 +1,21 @@
 package com.ar.main;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@MapperScan("com.ar.main.mapper")
 @SpringBootApplication
-public class MainApplication {
+public class MainApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MainApplication.class);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(MainApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
 
 }
